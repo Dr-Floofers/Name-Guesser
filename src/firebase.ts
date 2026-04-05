263576b7057b2b1de87e2ed044206b79a12f6569
Dr. Floofers <myloschouten@gmail.com>
2026-04-05 11:38:37 -0600

    add analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
+import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDDWIxJ5EObohy4hIOGfqnf23ooJrzk8yY",
@@ -8,7 +9,10 @@ const firebaseConfig = {
   projectId: "name-guesser-alp",
   storageBucket: "name-guesser-alp.firebasestorage.app",
   messagingSenderId: "212250011060",
-  appId: "1:212250011060:web:b153e61c1941d56841bee1"
+  appId: "1:212250011060:web:b153e61c1941d56841bee1",
+  measurementId: "G-PEC5M28TTM"
 }
 
-export const db = getDatabase(initializeApp(firebaseConfig))
+const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
+getAnalytics(app)
